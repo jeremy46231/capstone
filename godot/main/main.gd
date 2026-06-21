b074af270775d9dddb04818ffcf51a0de1c937eb
Sid Senthil <sidsenthil2010@gmail.com>
2026-06-21 10:25:22 -0700

    bring back other levels

--- godot/main/main.gd
+++ godot/main/main.gd
@@ -6,10 +6,10 @@ extends Node2D
 
 # ordered list of levels
 const LEVELS := [
-	# "res://levels/level_1.tscn",
-	# "res://levels/level_2.tscn",
-	# "res://levels/level_3.tscn",
-	# "res://levels/level_4.tscn",
+	"res://levels/level_1.tscn",
+	"res://levels/level_2.tscn",
+	"res://levels/level_3.tscn",
+	"res://levels/level_4.tscn",
 	"res://levels/end.tscn"
 ]
 
