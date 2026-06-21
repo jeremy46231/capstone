e4da2a82aa15b0d9eab70116b0c62dbb89fb0510
Sid Senthil <sidsenthil2010@gmail.com>
2026-06-21 09:23:12 -0700

    remove quit

--- godot/title/title.gd
+++ godot/title/title.gd
@@ -22,7 +22,3 @@ func _on_quit_pressed() -> void:
 
 func _on_play_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main/main.tscn")
-
-
-func _on_quit_texture_button_pressed() -> void:
-	get_tree().quit()
